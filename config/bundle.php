e7016742c17ce79140bddca460100e654a408eed
sage5413 <sage5413@163.com>
2017-03-16 10:37:09 +0800

    1

--- config/bundle.php
+++ config/bundle.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . "/../app/ApiBundle/Bundle.php";
- require __DIR__ . "/../app/PayBundle/Bundle.php";
+require __DIR__ . "/../app/PayBundle/Bundle.php";
 require __DIR__ . "/../app/WebBundle/Bundle.php";
 require __DIR__ . "/../app/WapBundle/Bundle.php";
 require __DIR__ . "/../app/WeixinBundle/Bundle.php";
